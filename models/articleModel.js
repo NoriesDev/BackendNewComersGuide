73441e5be0425999af56d18d38ca61e156774940
RockandRolls <livelydev010@gmail.com>
2023-11-30 11:26:11 +0100

    restoring

---
+++ models/articleModel.js
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+
+const articleSchema = new Schema({
+    title: {
+      type: String,
+      maxlength: 255,
+      match: [
+        /^[a-zA-Z\s.-]+$/,
+        'must contain only letters and max 255 characters long',
+      ],
+    },
+    imgSrc: [{
+      type: String,
+      required: true,
+      match: [
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+        'must be a valid URL',
+      ],
+    }],
+    article: {
+      type: String,
+    },
+    usefulLinks: [{
+        type: String,
+        match: [
+          /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+          'must be a valid URL',
+        ],
+      }],
+  });
+  
+  export default model('Article', articleSchema);
