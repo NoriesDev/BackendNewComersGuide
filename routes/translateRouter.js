abca58d58e804f55bdee51fc6e5f50ffecdd6766
RockandRolls <livelydev010@gmail.com>
2023-12-01 16:02:35 +0100

    multiLang

--- routes/translateRouter.js
+++ routes/translateRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {translateText, allLanguages, translateDocument} from '../controllers/translationController.js';
 import upload from '../middleware/multerUpload.js';
+import deeplLanguages from '../middleware/deeplLanguages.js';
 
 const translateRouter = express.Router();
 
 translateRouter.post('/', translateText);
 
-translateRouter.get('/languages', allLanguages);
+translateRouter.get('/languages', deeplLanguages, allLanguages);
 
-translateRouter.post('/document', upload.single('translateDocument'), translateDocument);
+translateRouter.post('/document', upload.single('translateDocument'), deeplLanguages, translateDocument);
 
 export default translateRouter;
