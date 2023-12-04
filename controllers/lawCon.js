53fecc447e1e5dc167e411b4f1bca70af5bb87b8
NoriesDev <noriesdevelopment@gmail.com>
2023-12-04 11:49:03 +0100

    commit for update

--- controllers/lawCon.js
+++ controllers/lawCon.js
@@ -3,7 +3,7 @@ import LawModel from '../models/lawModel.js';
 
 const allLaws = async (req, res, next) => {
     try {
-        const dbLaw = await LawModel.find();
+        const dbLaw = await LawModel.find().populate('article');
         return res.json(dbLaw)
     } catch (error) {
         next(error);
@@ -14,7 +14,7 @@ const oneLaw = async (req, res, next) => {
     try {
         const { id } = req.params;
         if (!id.match(/^[a-f\d]{24}$/i)) throw new ErrorStatus('Can not find Id', 400);
-        const findLaw = await LawModel.findById(id);
+        const findLaw = await LawModel.findById(id).populate('article');
         return res.json(findLaw)
     } catch (error) {
         next(error);
@@ -23,14 +23,12 @@ const oneLaw = async (req, res, next) => {
 
 const createLaw = async (req, res, next) => {
     try {
-        const { title, law, article, imgSrc} = req.body;
-        if (!title || !law || !article || !imgSrc)
+        const { imgSrc, article } = req.body;
+        if ( !imgSrc || !article || !Array.isArray(article))
         throw new ErrorStatus('Please make sure you have all content you need', 400);
         const newLaw = await LawModel.create({
-            title,
-            law, 
-            article,
-            imgSrc
+            imgSrc,
+            article
         });
         return res.json(newLaw);
     } catch (error) {
